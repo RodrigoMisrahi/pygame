8b65d9796f6b3c4add2963e40cfac568057b3ce5
guilhermeam8 <guilhermeam8@al.insper.edu.br>
2025-05-21 15:49:29 -0300

    ranking

--- joguinho.py
+++ joguinho.py
@@ -1,5 +1,26 @@
 import pygame
 import random
+import ranking
+import json
+
+def carregar_rankings():
+    try:
+        with open('rankings.json', 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+
+def salvar_rankings(rankings):
+    with open('rankings.json', 'w') as f:
+        json.dump(rankings, f)
+
+def adicionar_pontuacao(nome, pontuacao):
+    rankings = carregar_rankings()
+    rankings.append({'nome': nome, 'pontuacao': pontuacao})
+    # Ordena do maior para o menor e mantém apenas os top 10
+    rankings.sort(key=lambda x: x['pontuacao'], reverse=True)
+    rankings = rankings[:10]
+    salvar_rankings(rankings)
 # Inicializa o Pygame
 pygame.init()
 
